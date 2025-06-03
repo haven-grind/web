0a7ee450381ae2d8c8954cb5e0a073938708988b
Muhammad Khalish Zhafran <khalish260303@gmail.com>
2025-06-04 01:42:04 +0700

    feat: add utils method for preview image and auto click file input

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function previewImage(file: File | undefined, elementId: string) {
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = (e) => {
+        const imgElement = document.getElementById(elementId) as HTMLImageElement;
+        if (imgElement) {
+            imgElement.src = e.target?.result as string;
+        }
+    };
+}
+
+export function clickInputFile(inputName: string) {
+    const inputElement = document.querySelector(`input[name="${inputName}"]`) as HTMLInputElement;
+    inputElement?.click();
+}
