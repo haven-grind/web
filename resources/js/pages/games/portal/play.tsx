7db1c5c2545472fff9c9c55cbcc357f0432f1c52
Muhammad Khalish Zhafran <khalish260303@gmail.com>
2025-06-04 01:45:14 +0700

    fix: modify ux structure and play game resolution

--- resources/js/pages/games/portal/play.tsx
+++ resources/js/pages/games/portal/play.tsx
@@ -49,11 +49,30 @@ export default function PlayGame({ game, similarGames }: { game: GameProps; simi
             <Head title={game.title} />
 
             <main className="container mx-auto px-4 py-8">
-                <div className="grid grid-cols-1 gap-8 lg:grid-cols-[2fr_1fr]">
-                    <div>
+                <div>
+                    <div className="mb-8">
+                        <Tabs defaultValue="play">
+                            <TabsList className="grid w-full grid-cols-3">
+                                <TabsTrigger value="play">Play Game</TabsTrigger>
+                                <TabsTrigger value="about">About</TabsTrigger>
+                                <TabsTrigger value="comments">Comments</TabsTrigger>
+                            </TabsList>
+                            <TabsContent value="play" className="mt-4">
+                                <GameContentPlay path={game.game_path} />
+                            </TabsContent>
+                            <TabsContent value="about" className="mt-4 space-y-4">
+                                <GameContentAbout descriptions={game.description.split('\n\n')} />
+                            </TabsContent>
+                            <TabsContent value="comments" className="mt-4">
+                                <GameContentComment comments={game.comments} />
+                            </TabsContent>
+                        </Tabs>
+                    </div>
+
+                    <div className="block md:flex md:space-x-10">
                         {game.thumbnail ? (
                             <div className="relative mb-6 aspect-video w-full overflow-hidden rounded-lg">
-                                <img src={game.thumbnail} alt={game.title} className="h-full w-full object-cover" />
+                                <img src={game.thumbnail} alt={game.title} className="h-full w-full border object-contain" />
                             </div>
                         ) : (
                             <div className="mb-6 flex aspect-video w-full items-center justify-center rounded-lg bg-gray-100 dark:bg-gray-800">
@@ -61,64 +80,51 @@ export default function PlayGame({ game, similarGames }: { game: GameProps; simi
                             </div>
                         )}
 
-                        <div className="mb-8">
-                            <Tabs defaultValue="play">
-                                <TabsList className="grid w-full grid-cols-3">
-                                    <TabsTrigger value="play">Play Game</TabsTrigger>
-                                    <TabsTrigger value="about">About</TabsTrigger>
-                                    <TabsTrigger value="comments">Comments</TabsTrigger>
-                                </TabsList>
-                                <TabsContent value="play" className="mt-4">
-                                    <GameContentPlay path={game.game_path} />
-                                </TabsContent>
-                                <TabsContent value="about" className="mt-4 space-y-4">
-                                    <GameContentAbout descriptions={game.description.split('\n\n')} />
-                                </TabsContent>
-                                <TabsContent value="comments" className="mt-4">
-                                    <GameContentComment comments={game.comments} />
-                                </TabsContent>
-                            </Tabs>
+                        <div className="md:flex-1/2">
+                            <GameDetail game={game} />
+                            <GameInformation developer={game.developer} releaseDate={game.createdAt} />
                         </div>
+                    </div>
 
-                        <div>
-                            <h2 className="mb-4 text-2xl font-bold">More Games Like This</h2>
-                            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
-                                {similarGames.length > 0 ? (
-                                    similarGames.map((game) => (
-                                        <GameCard
-                                            key={game.id}
-                                            id={game.id}
-                                            href={`/play/${game.id}`}
-                                            title={game.title}
-                                            thumbnail={game.thumbnail}
-                                            developer={game.developer}
-                                            genre={game.genres}
-                                        />
+                    <div className="mb-4 space-y-6">
+                        <div className="rounded-lg border p-6">
+                            <h3 className="mb-4 font-semibold">Screenshots</h3>
+                            <div className="grid grid-cols-2 gap-2 md:grid-cols-4">
+                                {game.screenshots?.length ? (
+                                    game.screenshots.map((screenshot, i) => (
+                                        <div key={i} className="relative aspect-video overflow-hidden rounded-md">
+                                            <img
+                                                src={screenshot}
+                                                alt={`Screenshot ${i + 1}`}
+                                                className="h-full w-full object-contain transition-transform duration-200 ease-in hover:scale-110"
+                                            />
+                                        </div>
                                     ))
                                 ) : (
-                                    <div className="col-span-2 text-gray-500 dark:text-gray-400">No Similar Games Available</div>
+                                    <div className="col-span-4 text-center text-gray-500 dark:text-gray-400">No Screenshots Available</div>
                                 )}
                             </div>
                         </div>
                     </div>
 
-                    <div className="space-y-6">
-                        <GameDetail game={game} />
-                        <GameInformation developer={game.developer} releaseDate={game.createdAt} />
-
-                        <div className="rounded-lg border p-6">
-                            <h3 className="mb-4 font-semibold">Screenshots</h3>
-                            <div className="grid grid-cols-2 gap-2">
-                                {game.screenshots?.length ? (
-                                    game.screenshots.map((screenshot, i) => (
-                                        <div key={i} className="relative aspect-video overflow-hidden rounded-md">
-                                            <img src={screenshot} alt={`Screenshot ${i + 1}`} className="object-cover" />
-                                        </div>
-                                    ))
-                                ) : (
-                                    <div className="col-span-2 text-center text-gray-500 dark:text-gray-400">No Screenshots Available</div>
-                                )}
-                            </div>
+                    <div className="space-y-4">
+                        <h2 className="text-2xl font-bold">More Games Like This</h2>
+                        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+                            {similarGames.length > 0 ? (
+                                similarGames.map((game) => (
+                                    <GameCard
+                                        key={game.id}
+                                        id={game.id}
+                                        href={`/play/${game.id}`}
+                                        title={game.title}
+                                        thumbnail={game.thumbnail}
+                                        developer={game.developer}
+                                        genre={game.genres}
+                                    />
+                                ))
+                            ) : (
+                                <div className="col-span-2 text-gray-500 dark:text-gray-400">No Similar Games Available</div>
+                            )}
                         </div>
                     </div>
                 </div>
