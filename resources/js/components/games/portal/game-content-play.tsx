9afb5135b58011b08203d4f7a91bc50117f0efe2
Muhammad Khalish Zhafran <khalish260303@gmail.com>
2025-06-04 14:02:04 +0700

    fix: set game display to fit aspect ratio 16/9

--- resources/js/components/games/portal/game-content-play.tsx
+++ resources/js/components/games/portal/game-content-play.tsx
@@ -5,8 +5,8 @@ export default function GameContentPlay({ path }: { path: string }) {
         <>
             {path ? (
                 <div className="overflow-hidden rounded-lg border">
-                    <div className="flex w-full items-center justify-center bg-gray-100 dark:bg-gray-800">
-                        <iframe src={path} className={`h-[${720}px] w-[${1280}px]`} title="Game iframe" scrolling={'no'} />
+                    <div className="flex aspect-[16/9] w-full items-center justify-center bg-gray-100 dark:bg-gray-800">
+                        <iframe src={path} className="h-full w-full" title="Game iframe" scrolling={'no'} />
                     </div>
                     <div className="flex items-center justify-between bg-gray-50 p-4 dark:bg-gray-900">
                         <span className="text-sm text-gray-500 dark:text-gray-400">Press ESC to exit fullscreen</span>
