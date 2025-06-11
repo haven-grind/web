f3a9b4edac52464eeca1891e9ea2b8129bf56f0f
Muhammad Khalish Zhafran <khalish260303@gmail.com>
2025-06-11 15:06:08 +0700

    feat: add menu checking for authed user

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -71,13 +71,11 @@ const rightNavItems: FooterNavItem[] = [
         title: 'Login',
         href: '/login',
         icon: LogIn,
-        className: '',
     },
     {
         title: 'Register',
         href: '/register',
         icon: UserPlus,
-        className: 'bg-pink-600 hover:bg-pink-700',
     },
 ];
 
@@ -91,6 +89,7 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
     const page = usePage<SharedData>();
     const { auth } = page.props;
     const getInitials = useInitials();
+    const isLoggedIn = auth.user !== null;
 
     const renderNavGroup = (item: SidebarNavItem) => {
         const isActive = item.items?.some((subItem) => page.url === subItem.href);
@@ -106,17 +105,21 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                 <NavigationMenuContent>
                     <ul className="grid w-[200px] gap-4">
                         <li>
-                            {item.items?.map((subItem) => (
-                                <NavigationMenuLink asChild key={subItem.title}>
-                                    <Link
-                                        href={subItem.href}
-                                        className={cn(page.url === subItem.href && activeItemStyles, 'flex-row items-center gap-2 px-3')}
-                                    >
-                                        {subItem.icon && <Icon iconNode={subItem.icon} />}
-                                        {subItem.title}
-                                    </Link>
-                                </NavigationMenuLink>
-                            ))}
+                            {item.items?.map((subItem) => {
+                                if (subItem.requiresAuth && !isLoggedIn) return null;
+
+                                return (
+                                    <NavigationMenuLink asChild key={subItem.title}>
+                                        <Link
+                                            href={subItem.href}
+                                            className={cn(page.url === subItem.href && activeItemStyles, 'flex-row items-center gap-2 px-3')}
+                                        >
+                                            {subItem.icon && <Icon iconNode={subItem.icon} />}
+                                            {subItem.title}
+                                        </Link>
+                                    </NavigationMenuLink>
+                                );
+                            })}
                         </li>
                     </ul>
                 </NavigationMenuContent>
@@ -152,16 +155,20 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                     </CollapsibleTrigger>
                     <CollapsibleContent>
                         <SidebarMenuSub>
-                            {item.items?.map((subItem) => (
-                                <SidebarMenuSubItem key={subItem.title}>
-                                    <SidebarMenuSubButton asChild className={cn(page.url === subItem.href && activeItemStyles)}>
-                                        <Link href={subItem.href}>
-                                            {subItem.icon && <Icon iconNode={subItem.icon} className="h-4 w-4" />}
-                                            {subItem.title}
-                                        </Link>
-                                    </SidebarMenuSubButton>
-                                </SidebarMenuSubItem>
-                            ))}
+                            {item.items?.map((subItem) => {
+                                if (subItem.requiresAuth && !isLoggedIn) return null;
+
+                                return (
+                                    <SidebarMenuSubItem key={subItem.title}>
+                                        <SidebarMenuSubButton asChild className={cn(page.url === subItem.href && activeItemStyles)}>
+                                            <Link href={subItem.href}>
+                                                {subItem.icon && <Icon iconNode={subItem.icon} className="h-4 w-4" />}
+                                                {subItem.title}
+                                            </Link>
+                                        </SidebarMenuSubButton>
+                                    </SidebarMenuSubItem>
+                                );
+                            })}
                         </SidebarMenuSub>
                     </CollapsibleContent>
                 </SidebarMenuItem>
@@ -203,6 +210,9 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                                         <SidebarGroup>
                                             <SidebarMenu>
                                                 {mainNavItems.map((item) => {
+                                                    if ((item.requiresAuth || item.items?.every((subItem) => subItem.requiresAuth)) && !isLoggedIn)
+                                                        return null;
+
                                                     if (item.items && item.items.length > 0) return renderMobileNavGroup(item);
                                                     return renderMobileNavItem(item);
                                                 })}
@@ -223,6 +233,8 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                         <NavigationMenu viewport={false} className="flex h-full items-stretch">
                             <NavigationMenuList className="flex h-full items-stretch space-x-2">
                                 {mainNavItems.map((item) => {
+                                    if ((item.requiresAuth || item.items?.every((subItem) => subItem.requiresAuth)) && !isLoggedIn) return null;
+
                                     if (item.items && item.items.length > 0) return renderNavGroup(item);
                                     return renderNavItem(item);
                                 })}
@@ -235,44 +247,50 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                             <Button variant="ghost" size="icon" className="group h-9 w-9 cursor-pointer">
                                 <Search className="!size-5 opacity-80 group-hover:opacity-100" />
                             </Button>
-                            <div className="hidden lg:flex">
-                                {rightNavItems.map((item) => (
-                                    <TooltipProvider key={item.title} delayDuration={0}>
-                                        <Tooltip>
-                                            <TooltipTrigger>
-                                                <a
-                                                    href={item.href}
-                                                    target="_blank"
-                                                    rel="noopener noreferrer"
-                                                    className="group text-accent-foreground ring-offset-background hover:bg-accent hover:text-accent-foreground focus-visible:ring-ring ml-1 inline-flex h-9 w-9 items-center justify-center rounded-md bg-transparent p-0 text-sm font-medium transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
-                                                >
-                                                    <span className="sr-only">{item.title}</span>
-                                                    {item.icon && <Icon iconNode={item.icon} className="size-5 opacity-80 group-hover:opacity-100" />}
-                                                </a>
-                                            </TooltipTrigger>
-                                            <TooltipContent>
-                                                <p>{item.title}</p>
-                                            </TooltipContent>
-                                        </Tooltip>
-                                    </TooltipProvider>
-                                ))}
-                            </div>
+
+                            {!isLoggedIn && (
+                                <div className="flex">
+                                    {rightNavItems.map((item) => (
+                                        <TooltipProvider key={item.title} delayDuration={0}>
+                                            <Tooltip>
+                                                <TooltipTrigger>
+                                                    <a
+                                                        href={item.href}
+                                                        className="group text-accent-foreground ring-offset-background hover:bg-accent hover:text-accent-foreground focus-visible:ring-ring ml-1 inline-flex h-9 w-9 items-center justify-center rounded-md bg-transparent p-0 text-sm font-medium transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
+                                                    >
+                                                        <span className="sr-only">{item.title}</span>
+                                                        {item.icon && (
+                                                            <Icon iconNode={item.icon} className="size-5 opacity-80 group-hover:opacity-100" />
+                                                        )}
+                                                    </a>
+                                                </TooltipTrigger>
+                                                <TooltipContent>
+                                                    <p>{item.title}</p>
+                                                </TooltipContent>
+                                            </Tooltip>
+                                        </TooltipProvider>
+                                    ))}
+                                </div>
+                            )}
                         </div>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                                <Button variant="ghost" className="size-10 rounded-full p-1">
-                                    <Avatar className="size-8 overflow-hidden rounded-full">
-                                        <AvatarImage src={auth.user.avatar} alt={auth.user.name} />
-                                        <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
-                                            {getInitials(auth.user.name)}
-                                        </AvatarFallback>
-                                    </Avatar>
-                                </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent className="w-56" align="end">
-                                <UserMenuContent user={auth.user} />
-                            </DropdownMenuContent>
-                        </DropdownMenu>
+
+                        {isLoggedIn && (
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Button variant="ghost" className="size-10 rounded-full p-1">
+                                        <Avatar className="size-8 overflow-hidden rounded-full">
+                                            <AvatarImage src={auth.user.avatar} alt={auth.user.name} />
+                                            <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                                {getInitials(auth.user.name)}
+                                            </AvatarFallback>
+                                        </Avatar>
+                                    </Button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent className="w-56" align="end">
+                                    <UserMenuContent user={auth.user} />
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        )}
                     </div>
                 </div>
             </div>
